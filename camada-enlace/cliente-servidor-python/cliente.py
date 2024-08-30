06b3c787a9a4c201d9eb373f46abbb0a9d231025
CAMPOS Leonardo <leonardo.campos@hexagon.com>
2024-08-30 12:01:15 -0300

    atualiza

---
+++ camada-enlace/cliente-servidor-python/cliente.py
@@ -0,0 +1,11 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 65432   
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT)) 
+    s.sendall('Olá, servidor'.encode('utf-8')) 
+    data = s.recv(1024)  
+
+print(f'Recebido do servidor: {data.decode()}')
