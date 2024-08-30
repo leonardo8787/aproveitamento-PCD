53459161783eadfc08f2e6da53da0f5ab785e20d
CAMPOS Leonardo <leonardo.campos@hexagon.com>
2024-08-30 12:02:30 -0300

    atualiza

--- camada-enlace/cliente-servidor-python/cliente.py
+++ camada-enlace/cliente-servidor-python/cliente.py
@@ -5,7 +5,7 @@ PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT)) 
-    s.sendall('Olá, servidor'.encode('utf-8')) 
+    s.sendall('Ola, servidor'.encode('utf-8')) 
     data = s.recv(1024)  
 
 print(f'Recebido do servidor: {data.decode()}')
--- camada-enlace/controle-congestionamento/main.py
+++ camada-enlace/controle-congestionamento/main.py
@@ -3,26 +3,24 @@ import time
 
 class CongestionControl:
     def __init__(self, max_window_size):
-        self.window_size = 1  # Tamanho inicial da janela
-        self.max_window_size = max_window_size  # Tamanho máximo da janela
-        self.ssthresh = max_window_size / 2  # Limiar de transição para a fase de congestionamento
-        self.state = 'slow_start'  # Estado inicial
-        self.data_sent = 0  # Dados enviados
-        self.data_acked = 0  # Dados confirmados
+        self.window_size = 1 
+        self.max_window_size = max_window_size 
+        self.ssthresh = max_window_size / 2 
+        self.state = 'slow_start' 
+        self.data_sent = 0
+        self.data_acked = 0 
     
     def send_data(self):
-        # Simula o envio de dados com base na janela de congestionamento
+
         print(f"Enviando {self.window_size} pacotes")
         self.data_sent += self.window_size
     
     def receive_ack(self):
-        # Simula o recebimento de uma confirmação de pacotes
         acks = random.randint(1, self.window_size)
         self.data_acked += acks
         print(f"Recebido {acks} confirmações")
     
     def adjust_window(self):
-        # Ajusta o tamanho da janela com base no estado e confirmações recebidas
         if self.state == 'slow_start':
             if self.data_acked >= self.window_size:
                 self.window_size = min(self.window_size * 2, self.max_window_size)
@@ -43,11 +41,9 @@ class CongestionControl:
             self.send_data()
             self.receive_ack()
             self.adjust_window()
-            time.sleep(1)  # Simula o tempo de rede
+            time.sleep(1)  
 
-# Parâmetros do controle de congestionamento
 max_window_size = 16
 congestion_control = CongestionControl(max_window_size)
 
-# Simular o controle de congestionamento
 congestion_control.simulate(rounds=10)
